7e571967994e9f94b46f0cedf1ac667aaab4f606
matheus_cascalho <cascalhom@gmail.com>
2021-04-15 22:30:08 -0300

    passo 1 - testes tradicionais

--- tests/test_selic.py
+++ tests/test_selic.py
@@ -33,3 +33,97 @@ import pytest
 from datetime import datetime
 
 
+# testes tradicionais
+def test_selic_valor_total():
+    # arrange
+    valor_aplicado = 100
+    periodo = 30
+    rentabilidade = 0.2
+
+    expected = 100.2
+
+    # act
+    result = total_com_juros(
+        valor_aplicado=valor_aplicado,
+        periodo=periodo,
+        rentabilidade=rentabilidade
+    )
+
+    # assert
+    assert result == expected
+
+
+def test_selic_imposto_a_pagar_30_dias():
+    # arrange
+    valor_aplicado = 100
+    periodo = 30
+    rentabilidade = 0.2
+
+    expected = 0.05  # 0.2 * 0.225
+
+    # act
+    result = imposto_a_pagar(
+        valor_aplicado=valor_aplicado,
+        periodo=periodo,
+        rentabilidade=rentabilidade
+    )
+
+    # assert
+    assert result == expected
+
+
+def test_selic_imposto_a_pagar_181_dias():
+    # arrange
+    valor_aplicado = 100
+    periodo = 181
+    rentabilidade = 0.2
+
+    expected = 0.24  # 1.21 * 0.2
+
+    # act
+    result = imposto_a_pagar(
+        valor_aplicado=valor_aplicado,
+        periodo=periodo,
+        rentabilidade=rentabilidade
+    )
+
+    # assert
+    assert result == expected
+
+
+def test_selic_imposto_a_pagar_365_dias():
+    # arrange
+    valor_aplicado = 100
+    periodo = 365
+    rentabilidade = 0.2
+
+    expected = 0.43  # 2.43 * 0.175
+
+    # act
+    result = imposto_a_pagar(
+        valor_aplicado=valor_aplicado,
+        periodo=periodo,
+        rentabilidade=rentabilidade
+    )
+
+    # assert
+    assert result == expected
+
+
+def test_selic_imposto_a_pagar_730_dias():
+    # arrange
+    valor_aplicado = 100
+    periodo = 730
+    rentabilidade = 0.2
+
+    expected = 0.75  # 4.91 * 0.15
+
+    # act
+    result = imposto_a_pagar(
+        valor_aplicado=valor_aplicado,
+        periodo=periodo,
+        rentabilidade=rentabilidade
+    )
+
+    # assert
+    assert result == expected
